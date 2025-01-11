02a1a3d5fa4054e159c18526476524660f1d759a
kirstennicole422 <kirstennicole422@gmail.com>
2025-01-11 11:21:51 -0700

    Week 1 Exercise 4 Work

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Data;
+using System.Net.Http.Headers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        string userInput = "";
+        int nextNumber;
+        List<int> numbers = new List<int>();
+        float sum = 0;
+        int max = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            if (userInput == "0")
+            {
+                break;
+            }
+            nextNumber = int.Parse(userInput);
+            numbers.Add(nextNumber);
+        } while (userInput != "0");
+
+
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+            if (number > max)
+            {
+                max = number;
+            }
+            sum = sum + number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum/numbers.Count}");
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
