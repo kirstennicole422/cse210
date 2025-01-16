0aeb2e388206e805b8f7b693a529c9b430f20938
kirstennicole422 <kirstennicole422@gmail.com>
2025-01-15 18:55:59 -0700

    resume loop fixed

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -7,17 +7,11 @@ public class Resume
     {
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
-        
-        // for (int i = 0; i < _jobs.Count; i ++)
-        // {
-        //     Console.WriteLine($"{_jobs[i].DisplayJobDetails}");
-        // }
 
-
-        // foreach (Job j in Jobs._jobs)
-        // {
-        //     Console.WriteLine ($"{j.DisplayJobDetails()}");
-        // }
+        foreach (Job j in _jobs)
+        {
+            j.DisplayJobDetails();
+        }
 
     }
 }
